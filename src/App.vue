a506b1182617eae5fc88016dcd9bd97ff1cc3dfa
Bradley Exton <bradleyexton@MacBook-Pro.local>
2020-09-30 22:20:41 -0400

    Add Header styles

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-    <header>Vue Movies</header>
+    <header
+      class="w-full bg-teal-100 pl-16 py-3 text-3xl flex font-mono shadow"
+    >
+      <h3 class="text-pink-600">VUE</h3>
+      <h3 class="text-pink-900 ">Movies</h3>
+    </header>
     <div>
       <MovieList />
       <MovieDetails />
