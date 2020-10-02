bbac8c64abcf97095948202b4d14b732d19a949f
Bradley Exton <bradleyexton@MacBook-Pro.local>
2020-10-02 01:04:50 -0400

    add Icons and Icon system

---
+++ src/components/icons/IconBase.vue
@@ -0,0 +1,49 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="width"
+    :height="height"
+    x="0px"
+    y="0px"
+    viewBox="0 0 115.46 122.88"
+    style="enable-background:new 0 0 115.46 122.88"
+    :aria-labelledby="iconName"
+    role="presentation"
+  >
+    <title :id="iconName" lang="en">{{ iconName }} icon</title>
+    <g :fill="iconColor">
+      <slot />
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    iconName: {
+      type: String,
+      default: "box",
+    },
+    width: {
+      type: [Number, String],
+      default: 115.46,
+    },
+    height: {
+      type: [Number, String],
+      default: 122.88,
+    },
+    iconColor: {
+      type: String,
+      default: "currentColor",
+    },
+  },
+};
+</script>
+
+<style scoped>
+svg {
+  display: inline-block;
+  vertical-align: baseline;
+  margin-bottom: -2px; /* yes, I'm that particular about formatting */
+}
+</style>
