07f98218de4746432460cdf88e70ba81431a7c84
Bradley Exton <bradleyexton@MacBook-Pro.local>
2020-10-05 11:42:32 -0400

    add 'add movie' button to movie details card

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,72 @@
 <template>
-  <div id="app" class="h-screen">
+  <div id="app" class="h-screen flex flex-col bg-gray-200">
     <header
-      class="w-full bg-teal-100 pl-16 py-3 text-3xl flex font-mono shadow z-10"
+      class="w-full px-20 h-20 flex flex-none justify-between items-center shadow z-10"
     >
-      <h3 class="text-pink-600">VUE</h3>
-      <h3 class="text-pink-900 ">Movies</h3>
+      <div class="text-3xl font-mono font-bold flex">
+        <h3 class="text-pink-600 text-3xl font-mono font-bold">VUE</h3>
+        <h3 class="text-pink-900 mr-1 text-3xl font-mono font-bold">Movies</h3>
+
+        <icon-base width="38" height="38" icon-name="movie" iconColor="#702459">
+          <movie-icon />
+        </icon-base>
+      </div>
+      <form class="flex justify-end h-10" @submit.prevent="searchForMovie">
+        <input
+          class="shadow appearance-none border rounded w-64 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="Search"
+          v-model="searchTitle"
+          type="text"
+          placeholder="Movie Title..."
+        /><button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-5 rounded"
+        >
+          Search
+        </button>
+      </form>
     </header>
-    <div class="flex h-full">
-      <MovieDetails @search:movie="searchForMovie" />
-      <MovieList />
-    </div>
+    <MovieDetails
+      :activeMovie="activeMovie"
+      :displayMovieDetails="displayMovieDetails"
+      @add-movie="addActiveMovieToList"
+    />
+    <MovieList :movies="movies" />
   </div>
 </template>
 
 <script>
 import MovieList from "./components/MovieList";
 import MovieDetails from "./components/MovieDetails";
+import IconBase from "./components/icons/IconBase";
+import MovieIcon from "./components/icons/MovieIcon";
 
 export default {
   name: "App",
-  components: { MovieList, MovieDetails },
+  components: { MovieList, MovieDetails, IconBase, MovieIcon },
   data() {
     return {
       activeMovie: {},
+      movies: [],
+      searchTitle: "",
+      displayMovieDetails: false,
     };
   },
   methods: {
-    async searchForMovie(title) {
+    async searchForMovie() {
       try {
         const response = await fetch(
-          `http://www.omdbapi.com/?i=tt3896198&apikey=f7842b9a&t='${title}'`
+          `http://www.omdbapi.com/?i=tt3896198&apikey=f7842b9a&t='${this.searchTitle}'`
         );
         const data = await response.json();
-        console.log("Data: ", data);
         this.activeMovie = data;
+        this.displayMovieDetails = true;
       } catch (error) {
+        this.displayMovieDetails = false;
         console.error(error);
       }
+    },
+    addActiveMovieToList() {
+      this.movies = [...this.movies, this.activeMovie];
     },
   },
 };
--- src/components/MovieDetails.vue
+++ src/components/MovieDetails.vue
@@ -1,35 +1,57 @@
 <template>
-  <div class="flex-grow mx-16 my-5">
-    <form class="flex justify-end" @submit.prevent="handleSearch">
-      <input
-        class="shadow appearance-none border rounded w-64 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        id="Search"
-        v-model="searchTitle"
-        type="text"
-        placeholder="Movie Title..."
-      /><button
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-5 rounded"
-      >
-        Search
-      </button>
-    </form>
-  </div></template
->
+  <div class="pt-5 pb-16 bg-gray-200 shadow">
+    <div
+      v-show="displayMovieDetails"
+      class="py-8 flex text-gray-700 rounded shadow-2xl p-10 mt-5 bg-white mx-20"
+    >
+      <img class="shadow" :src="activeMovie.Poster" />
+      <div class="ml-10">
+        <p class="font-bold text-xl ml-12 mb-3 text-gray-900 font-mono">
+          {{ activeMovie.Title }}
+        </p>
+        <p class="flex items-center mb-5">
+          <icon-base class="mt-1" width="35" height="35" icon-name="director">
+            <director-icon />
+          </icon-base>
+          <span class="ml-3">{{ activeMovie.Director }}</span>
+        </p>
+        <p class="flex items-center mb-6">
+          <icon-base class="mt-1" width="35" height="35" icon-name="actors">
+            <actors-icon />
+          </icon-base>
+          <span class="ml-3">{{ activeMovie.Actors }}</span>
+        </p>
+        <p class="flex items-center mb-5">
+          <icon-base width="35" height="35" icon-name="release">
+            <date-icon />
+          </icon-base>
+          <span class="ml-3">{{ activeMovie.Year }}</span>
+        </p>
+        <p class="mt-10 ml-10 pl-1">{{ activeMovie.Plot }}</p>
+        <button
+          @click="$emit('add-movie')"
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-10 rounded mt-10"
+        >
+          Add To List
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
+import ActorsIcon from "./icons/ActorsIcon";
+import DateIcon from "./icons/DateIcon";
+import DirectorIcon from "./icons/DirectorIcon";
+import IconBase from "./icons/IconBase";
+
 export default {
   name: "movie-details",
-  methods: {
-    handleSearch() {
-      this.$emit("search:movie", this.searchTitle);
-    },
+  props: {
+    activeMovie: Object,
+    displayMovieDetails: Boolean,
   },
-
-  data() {
-    return {
-      searchTitle: "",
-    };
-  },
+  components: { ActorsIcon, DateIcon, DirectorIcon, IconBase },
 };
 </script>
 
